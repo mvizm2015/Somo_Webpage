71b2c4c0913ad059f54a5fa49e0343e50ece40e3
mvizm2015 <mariaviz2015@gmail.com>
2018-01-18 16:25:29 -0500

    built css html js

---
+++ index.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $(".material-icons").click(function(){
+    $("#bio-feed").html("Microdosing shoreditch post-ironic irony authentic mixtape bicycle rights YOLO. Franzen pour-over hoodie, four dollar toast ennui occupy biodiesel pitchfork fashion axe dreamcatcher tote bag. Pok pok master cleanse VHS, keffiyeh PBR&B subway tile hoodie flexitarian tote bag post-ironic health goth. Brunch tousled small batch XOXO air plant. Farm-to-table meh church-key, subway tile you probably haven't heard of them tumblr dreamcatcher. XOXO glossier gastropub master cleanse whatever, cold-pressed brooklyn knausgaard +1 tacos listicle shoreditch affogato. Farm-to-table meh church-key, subway tile you probably haven't heard of them tumblr dreamcatcher. XOXO glossier gastropub master cleanse whatever, cold-pressed brooklyn knausgaard +1 tacos listicle shoreditch affogato.");
+    $(this).click().animate({
+      'scrollTop': $target.offset().top
+    }, 300, 'swing');
+  });
+});
+
+// 
+// $(document).ready(function(){
+//   $('html, body').animate(function() {
+//     $("#bio-feed").html("Microdosing shoreditch post-ironic irony authentic mixtape bicycle rights YOLO. Franzen pour-over hoodie, four dollar toast ennui occupy biodiesel pitchfork fashion axe dreamcatcher tote bag. Pok pok master cleanse VHS, keffiyeh PBR&B subway tile hoodie flexitarian tote bag post-ironic health goth. Brunch tousled small batch XOXO air plant. Farm-to-table meh church-key, subway tile you probably haven't heard of them tumblr dreamcatcher. XOXO glossier gastropub master cleanse whatever, cold-pressed brooklyn knausgaard +1 tacos listicle shoreditch affogato. Farm-to-table meh church-key, subway tile you probably haven't heard of them tumblr dreamcatcher. XOXO glossier gastropub master cleanse whatever, cold-pressed brooklyn knausgaard +1 tacos listicle shoreditch affogato.");
+//   });
